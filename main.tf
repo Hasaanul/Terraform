4adbfb4ce928fb712c5fae26bde05888a6f6ee6e
EC2 Default User <ec2-user@ip-172-31-87-250.ec2.internal>
2024-03-25 22:14:13 +0000

    adding terraform main.yml

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "Rhel" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "RHEL-Server" {
+  count         = 1
+  ami           = "ami-0fe630eb857a6ec83"
+  instance_type = "t2.medium"
+  key_name      = "rhelkeypair1"
+  tags = {
+    Name = "RHEL-Server"
+  }
+}
+
+
+provider "Ubuntu" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "Ubuntu-Server" {
+  count         = 1
+  ami           = "ami-080e1f13689e07408"
+  instance_type = "t2.medium"
+  key_name      = "rhelkeypair1"
+  tags = {
+    Name = "Ubuntu-Server"
+  }
+}
+
+
